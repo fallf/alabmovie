c1753659f884691778e5b9c45c417e7847b2e803
fatou fall <fatoufall95@gmail.com>
2024-12-13 15:31:42 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+function App() {
+  //Constant with your API key
+  const apiKey = "14aa79a1";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async (searchTerm) => {
+    try {
+      // Make fetch request and store the response
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      // Parse JSON response into a JavaScript object
+      const data = await response.json();
+      // Set the Movie state to the received data
+      setMovie(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getMovie("Guardians of the Galaxy Vol. 2");
+  }, []);
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
+
+export default App;
